776c7ff6f3c0f7069d1f639b5cf2aaecab7caf8f
lesia_onyshchuk <lkane443@gmail.com>
2025-03-03 11:05:43 +0200

    fix all bugs

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,50 +1,7 @@
-import { Field, Form, Formik } from "formik";
-import toast from "react-hot-toast";
-import { useDispatch } from "react-redux";
-import { NavLink, useNavigate } from "react-router-dom";
-import { loginThunk } from "../../redux/auth/operations";
-import css from "./LogIn.module.css";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 const LogIn = () => {
-  const initialValues = {
-    email: "",
-    password: "",
-  };
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const handleSubmit = (values, options) => {
-    console.log(values);
-    dispatch(loginThunk(values))
-      .unwrap()
-      .then((res) => {
-        toast.success(`Welcome, ${res.user.email}`);
-        navigate("/contacts", { replace: true });
-      })
-      .catch(() => toast.error("You do not have account yet!"));
-
-    options.resetForm();
-  };
-  return (
-    <div>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        <Form className={css.form}>
-          <label>Email:</label>
-          <Field className={css.field} name="email" />
-          <label>Password:</label>
-          <Field className={css.field} name="password" type="password" />
-          <button className={css.loginButton} type="submit">
-            Login
-          </button>
-          <p>
-            You do not have account yet!
-            <NavLink className={css.link} to="/register">
-              Create account
-            </NavLink>
-          </p>
-        </Form>
-      </Formik>
-    </div>
-  );
+  return <LoginForm />;
 };
 
 export default LogIn;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,46 +1,6 @@
-import { Field, Form, Formik } from "formik";
-import { useDispatch } from "react-redux";
-import { registerThunk } from "../../redux/auth/operations";
-import { NavLink } from "react-router-dom";
-import css from "./Register.module.css";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
 
 const Register = () => {
-  const dispatch = useDispatch();
-
-  const initialValues = {
-    email: "",
-    name: "",
-    password: "",
-  };
-
-  const handleSubmit = (values, options) => {
-    console.log(values);
-    dispatch(registerThunk(values));
-    options.resetForm();
-  };
-
-  return (
-    <div>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        <Form className={css.form}>
-          <label>Name:</label>
-          <Field className={css.field} name="name" />
-          <label>Email:</label>
-          <Field className={css.field} name="email" />
-          <label>Password:</label>
-          <Field className={css.field} name="password" type="password" />
-          <button className={css.registerButton} type="submit">
-            Register
-          </button>
-          <p>
-            You already have account!
-            <NavLink className={css.link} to="/login">
-              Log in
-            </NavLink>
-          </p>
-        </Form>
-      </Formik>
-    </div>
-  );
+  return <RegistrationForm />;
 };
 export default Register;
